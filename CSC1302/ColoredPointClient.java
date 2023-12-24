b815a01d9a3aba8b52178963a08821fe37582ca9
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2023-12-24 15:50:42 -0500

    Update and rename ColoredPointClient.java to CSC1302/ColoredPointClient.java

--- CSC1302/ColoredPointClient.java
+++ CSC1302/ColoredPointClient.java
@@ -1,9 +1,7 @@
 package com.CSC1302;
 //Name: Talha Ansari
-//PantherID: 002-587-754
-//Due Date: July 20
 public class ColoredPointClient {
-//this program construct 2 ColoredPoint objects, compares them, and prints them out
+//this program constructs 2 ColoredPoint objects, compares them, and prints them out
     public static void main(String[] args) {
         ColoredPoint c = new ColoredPoint(9, 20, "GREEN");
         ColoredPoint g = new ColoredPoint(19, 29, "GREEN");

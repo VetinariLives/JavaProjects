ed442f72cef60bda57b9b89b37400976b2726d0d
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2023-12-24 15:49:52 -0500

    Update and rename Colored.java to CSC1302/Colored.java

--- CSC1302/Colored.java
+++ CSC1302/Colored.java
@@ -1,6 +1,4 @@
 package com.CSC1302;
 //Name: Talha Ansari
-//PantherID: 002-587-754
-//Due Date: July 20
 //This program creates the interface Colored, which has 1 instance method: getColor
 public interface Colored{public String getColor();}

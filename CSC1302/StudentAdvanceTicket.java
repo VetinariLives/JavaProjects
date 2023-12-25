9c81688eb11e1d3a58d591ea1c077f1bdcc7da45
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2023-12-24 22:20:36 -0500

    Update StudentAdvanceTicket.java

--- CSC1302/StudentAdvanceTicket.java
+++ CSC1302/StudentAdvanceTicket.java
@@ -1,7 +1,5 @@
 package com.CSC1302;
 //Name: Talha Ansari
-//PantherID: 002-587-754
-//Due Date: July 20
 public class StudentAdvanceTicket extends AdvanceTicket{/*
     This program creates a class of StudentAdvanceTicket objects, and is a subclass of AdvanceTicket.java
     The class has a constructor, which accepts 2 ints as a parameter

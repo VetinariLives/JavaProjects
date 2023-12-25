78d5403a91202cb6e143c95efef32e86f91983ac
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2023-12-24 22:55:35 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.CSC2720;
+public class Main {
+    public static void main(String[] args) {
+        QueueStack test = new QueueStack();
+        test.push(5);
+        test.push(10);
+        test.push(20);
+        test.push(30);
+        test.push(40);
+        while (!test.isEmpty()) {
+            System.out.println(test.In +" "+ test.Out);
+            System.out.println(test.pop());
+        }
+    }
+}

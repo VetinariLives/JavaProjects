0a8e4f9bda4f7803a888858f40394326c3d1fc7d
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2023-12-24 22:56:24 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.CSC2720;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	HashTable arr = new HashTable(8);
+	arr.put(5, "fhuebfgue");
+	arr.put(6, "fhuebfgue");
+	arr.put(7, "fhuebfgue");
+	arr.put(8, "fhuebfgue");
+	arr.put(9, "fhuebfgue");
+		System.out.println(arr.get(8));
+    }
+}

68d10c5b3832524dce24b7c6c4e0850fbdb17f58
Talha Ansari <93846591+VetinariLives@users.noreply.github.com>
2023-12-24 22:50:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.CSC2720;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        LinkedList problemOne = new LinkedList();
+        problemOne.addLast(2);
+        problemOne.addLast(4);
+        problemOne.addLast(8);
+        problemOne.addFirst(-2);
+        problemOne.addFirst(-4);
+        problemOne.addLast(9);
+        problemOne.print();
+        System.out.println(problemOne.indexOf(4));
+        System.out.println(problemOne.contains(9));
+        for (int i = 0; i < 6; ++i) {
+            problemOne.deleteLast();
+            problemOne.print();
+        }
+
+        LinkedList problemTwo = new LinkedList();
+        problemTwo.addLast(2);
+        problemTwo.addLast(4);
+        problemTwo.addLast(8);
+        problemTwo.addFirst(-2);
+        problemTwo.addFirst(-4);
+        problemTwo.addLast(9);
+        problemTwo.print();
+        problemTwo.reverse();
+        problemTwo.print();
+
+    }
+}
